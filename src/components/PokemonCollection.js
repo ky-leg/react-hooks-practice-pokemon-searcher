f1dbbb785d427920a26f1eda83962274d957e523
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-01-16 16:28:09 -0500

    commit tree 8a785ae41f685914115207199b993312caf045be

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,29 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({pokemon, search}) {
+  // console.log(pokemon.map(monster => monster.name))
+
+  // if (search) 
+  // return null 
+  // else console.log(search)
+
+  const searchedPokemon = pokemon.filter(monster => {
+    if (search==="") 
+    return monster
+    else 
+    return monster.name.toUpperCase().includes(search.toUpperCase())
+  })
+    // search ? pokemon : pokemon
+  // pokemon.filter(monster => {
+  //   monster.name.toUpperCase().includes(search.toUpperCase())
+  // })
+
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      {searchedPokemon.map(monster => (
+        <PokemonCard key={monster.id} monster={monster} />
+      ))}
     </Card.Group>
   );
 }
